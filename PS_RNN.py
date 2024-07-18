c8ccd2a57765b435b31760c0f2fbac39cd4039b0
gmancuso01 <132796402+gmancuso01@users.noreply.github.com>
2024-07-18 04:53:51 +0100

    Última versión de PS_RNN.py
    Versión corriendo hoy 18/07/2024 (comentamos la línea con la predicción sobre el conjunto de testeo)

--- PS_RNN.py
+++ PS_RNN.py
@@ -4,15 +4,16 @@ import keras
 import os
 from preprocessing import Preprocessing
 from sklearn.preprocessing import StandardScaler
-from keras.callbacks import ModelCheckpoint, Callback
 from models import Train_LSTM
 from sklearn.model_selection import train_test_split
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
 start_time = time.time()
 
 path_BH = os.path.join('data', 'BH')
 path_NS = os.path.join('data', 'NS')
-preprocessor = Preprocessing() 
+preprocessor = Preprocessing()
 
 bin_factor = 100
 BH_powerspectra = preprocessor.array_collect(path_BH,
@@ -20,12 +21,11 @@ BH_powerspectra = preprocessor.array_collect(path_BH,
                                              BH = True)
 NS_powerspectra = preprocessor.array_collect(path_NS,
                                              bin_factor = bin_factor,
-                                             BH=False)
+                                             BH = False)
 
-powerspectra=np.vstack((BH_powerspectra,NS_powerspectra))
+powerspectra = np.vstack((BH_powerspectra,NS_powerspectra))
 
-
-nodes = 32768 
+nodes = 32768
 
 X_test = StandardScaler().fit_transform(powerspectra[:,1:2])
 y_test = powerspectra[:,3]
@@ -38,32 +38,15 @@ np.random.shuffle(powerspectra)
 X_train = powerspectra[:,1:2]
 y_train = powerspectra[:,3]
 
-# Define a custom callback to save the model at epoch 5
-class SaveModelAtEpoch5(Callback):
-    def on_epoch_end(self, epoch, logs=None):
-        if epoch + 1 == 5:  # Save model at epoch 5
-            self.model.save('model_epoch_5.h5')
-
-
-# Define the model checkpoint callback
-model_checkpoint = ModelCheckpoint(filepath='model_checkpoint.h5',
-                                    monitor='val_loss',
-                                    save_best_only=False,
-                                    save_weights_only=False,
-                                    mode='auto',
-                                    period=1,
-                                    verbose=1)
-
-# Combine the two callbacks
-callbacks = [model_checkpoint, SaveModelAtEpoch5()]
-
-test_loss, test_acc, prediction  = Train_LSTM(X_train, y_train,
-                                              X_val, y_val,
-                                              X_test, y_test,
-                                              nodes,
-                                              batch_size= 30,
-                                              num_features = 1,
-                                              epochs = 7)
+#test_loss, test_acc, prediction = Train_LSTM(X_train, y_train,
+test_loss, test_acc = Train_LSTM(X_train, y_train,
+                                 X_val, y_val,
+                                 X_test, y_test,
+                                 nodes,
+                                 batch_size=30,
+                                 num_features=1,
+                                 epochs=1,
+                                 load = True)
 
 end_time = time.time()
 print(f"Total time taken: {end_time - start_time} seconds\n")
