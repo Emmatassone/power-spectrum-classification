7198348cf01e4bfff3c076561acda7765511f27b
Emmanuel Tassone <53381024+Emmatassone@users.noreply.github.com>
2024-08-25 11:17:06 -0300

    Add files via upload

--- models/PS_RNN.py
+++ models/PS_RNN.py
@@ -1,8 +1,8 @@
 import numpy as np
 import time
 import os
-from preprocessing import Preprocessing
-from models import train_LSTM
+from preprocessing.preprocessing import Preprocessing
+from models.models import train_LSTM
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
@@ -14,22 +14,27 @@ path_NS = os.path.join('data', 'NS')
 preprocessor = Preprocessing(path_BH, path_NS)
 powerspectra = preprocessor.collect_all_NS_BH_data()
 
-nodes = 32768
-#nodes = preprocessor.shape[1]?
-rows = preprocessor.shape[0]
 
-shuffled_PS = np.random.shuffle(powerspectra)
 
+#delete error column if there is one
+powerspectra  = np.delete(powerspectra , 2, axis=2)
+
+NODES = preprocessor.nodes
+NUM_FEATURES = powerspectra.shape[2]-1
+NUM_FILES = powerspectra.shape[0]
+
+ps=np.copy(powerspectra)
+
+np.random.shuffle(powerspectra)
 #test_loss, test_acc, prediction = Train_LSTM(X_train, y_train,
-train_LSTM(X_train = shuffled_PS[ : , 1:2 ], y_train = shuffled_PS[ : , 3 ],
-            X_val = powerspectra[ :rows//2 , 1:2 ], y_val = powerspectra[ :rows//2 , 3 ],
-            X_test = powerspectra[ rows//2: , 1:2 ], y_test = powerspectra[ rows//2: , 3 ],
-            nodes = nodes,
-            batch_size = 30,
-            num_features = 1,
-            epochs = 1,
-            load = True)
+train_LSTM(X_train = powerspectra[ : , :, 0:2 ], y_train = np.mean(powerspectra[ :, : , 2 ], axis=1).reshape(-1,1),
+            X_val = ps[ :NUM_FILES//2 , :, 0:2 ], y_val = np.mean(ps[ :NUM_FILES//2 , : , 2 ], axis=1).reshape(-1,1),
+            X_test = ps[ NUM_FILES//2: , :, 0:2 ], y_test =  np.mean( ps[ NUM_FILES//2:, : , 2 ], axis=1).reshape(-1,1),
+            time_steps=NODES,
+            batch_size=5,
+            num_features=NUM_FEATURES,
+            epochs=2)
 
 end_time = time.time()
-print(f"Total time taken: {end_time - start_time} seconds\n")
-print(f"That's equal to: {(end_time - start_time)/3600} hours\n")
+print("Total time taken: {} seconds\n".format(round(end_time - start_time,2)))
+print("That's equal to: {} hours\n".format(round((end_time - start_time)/3600,2)))
