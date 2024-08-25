520c141ced8c89b079971d782767b4dd6b8c6561
Emmanuel Tassone <53381024+Emmatassone@users.noreply.github.com>
2024-08-25 11:14:04 -0300

    Add files via upload

--- models/models.py
+++ models/models.py
@@ -5,6 +5,26 @@ import tensorflow as tf
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 
+
+class SaveModelEveryNEpochs(tf.keras.callbacks.Callback):
+    def __init__(self, save_interval=2, filepath_template="model_epoch_{epoch:02d}.h5"):
+        super(SaveModelEveryNEpochs, self).__init__()
+        self.save_interval = save_interval
+        self.filepath_template = filepath_template
+
+    def on_epoch_end(self, epoch, logs=None):
+        if (epoch + 1) % self.save_interval == 0:
+            filepath = self.filepath_template.format(epoch=epoch + 1)
+            self.model.save(filepath)
+            print(f"Model saved to {filepath}")
+            
+def model_predictions(model_path,X_new):
+    model = load_model(model_path)
+    y_pred_prob = model.predict(X_new)
+    y_pred = (y_pred_prob > 0.5).astype(int)
+    # Evaluate the model on the test set
+    return y_pred
+
 def train_CNN(X_train, y_train, X_val, y_val, epochs=6, batch_size=64):
     model = Sequential([
         Conv1D(input_shape=(X_train.shape[1], X_train.shape[2]), filters=128,
@@ -30,10 +50,11 @@ def train_CNN(X_train, y_train, X_val, y_val, epochs=6, batch_size=64):
     return model
 
 def train_LSTM(X_train, y_train, X_val, y_val, X_test, y_test,
-               time_steps, batch_size=5, num_features=1, epochs=6, load=False):
-    # Model architecture
-    if load == False:
+               time_steps, batch_size=5, num_features=1, epochs=6, load=False,
+               save_interval=2):
 
+    if load:
+        # Model architecture
         model = Sequential([
             LSTM(units=64, input_shape=(time_steps, num_features)),
             Dropout(0.2),
@@ -41,29 +62,25 @@ def train_LSTM(X_train, y_train, X_val, y_val, X_test, y_test,
         ])
 
     else:
-        model = load_model('model_epoch_06.h5')
-
+        model = load_model('model_epoch_02.h5')
+        
+        
     model.compile(loss='binary_crossentropy',
                   optimizer='adam',
                   metrics=['accuracy'])
 
     # Define the model checkpoint callback
-    checkpoint_path = "model_epoch_{epoch:02d}.h5"
-    cp_callback = ModelCheckpoint(filepath=checkpoint_path,
-                                  save_weights_only=False,
-                                  save_freq='epoch',
-                                  verbose=1,
-                                  period=2)  # Save every 2 epochs
+    save_callback = SaveModelEveryNEpochs(save_interval=save_interval)
 
-    history = model.fit(X_train, y_train,
-                        epochs = epochs,
-                        batch_size = batch_size,
-                        validation_data = (X_val, y_val),
-                        callbacks = [cp_callback])
+    model.fit(X_train, y_train,
+                epochs=epochs,
+                batch_size=batch_size,
+                validation_data=(X_val, y_val),
+                callbacks=[save_callback])
+
 
     test_loss, test_acc = model.evaluate(X_test, y_test)
     print("Test Set loss: ",test_loss,", Test Set accuracy: ", test_acc)
-#    predictions = model.predict(X_test)
     return model
 
 def train_RF(X_train, y_train, X_val, y_val, batch_size=64):
@@ -72,7 +89,11 @@ def train_RF(X_train, y_train, X_val, y_val, batch_size=64):
     X_val=pd.DataFrame(X_val,columns=['freq','power'])
     y_val=pd.DataFrame(y_val,columns=['BH?'])
     X_test, y_test=pd.DataFrame(X_test,columns=['freq','power']),pd.DataFrame(y_test,columns=['BH?'])
-    model = RandomForestClassifier(n_estimators=200,min_samples_leaf=20,min_samples_split=50,n_jobs=30)
+    model = RandomForestClassifier(n_estimators=200,
+                                   min_samples_leaf=20,
+                                   min_samples_split=50,
+                                   n_jobs=30
+                                  )
     model.fit(X_train, y_train)
     return model
     
